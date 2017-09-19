820bb26f61c90aa1483f5f63ef40608bc0196475
Richard Deloge <richarddeloge@gmail.com>
2017-09-19 11:27:20 +0200

    admin form

--- src/symfony/Form/Type/ContentType.php
+++ src/symfony/Form/Type/ContentType.php
@@ -46,7 +46,7 @@ class ContentType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('author', DocumentType::class, ['class' => User::class, 'required'=>true, 'multiple'=>false]);
+        $builder->add('author', DocumentType::class, ['class' => User::class, 'required'=>true, 'multiple'=>false, 'choice_label'=>'username']);
         $builder->add('type', DocumentType::class, ['class' => Type::class, 'required'=>true, 'multiple'=>false]);
         $builder->add('categories', DocumentType::class, ['class' => Category::class, 'required'=>true, 'multiple'=>true]);
         $builder->add('title', TextType::class, ['required'=>true]);

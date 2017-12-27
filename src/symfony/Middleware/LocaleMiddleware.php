b0f143a7469f2484c4cdfa178797529680840575
Richard Deloge <richarddeloge@gmail.com>
2017-12-27 15:32:39 +0100

    locale middleware dedicated to symfony translator updating, test

---
+++ src/symfony/Middleware/LocaleMiddleware.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * East Website.
+ *
+ * LICENSE
+ *
+ * This source file is subject to the MIT license and the version 3 of the GPL3
+ * license that are bundled with this package in the folder licences
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to richarddeloge@gmail.com so we can send you a copy immediately.
+ *
+ *
+ * @copyright   Copyright (c) 2009-2017 Richard Déloge (richarddeloge@gmail.com)
+ *
+ * @link        http://teknoo.software/east/website Project website
+ *
+ * @license     http://teknoo.software/license/mit         MIT License
+ * @author      Richard Déloge <richarddeloge@gmail.com>
+ */
+
+namespace Teknoo\East\WebsiteBundle\Middleware;
+
+use Psr\Http\Message\ServerRequestInterface;
+use Symfony\Component\Translation\TranslatorInterface;
+use Teknoo\East\Foundation\Http\ClientInterface;
+use Teknoo\East\Foundation\Manager\ManagerInterface;
+use Teknoo\East\Foundation\Middleware\MiddlewareInterface;
+
+/**
+ * @license     http://teknoo.software/license/mit         MIT License
+ * @author      Richard Déloge <richarddeloge@gmail.com>
+ */
+class LocaleMiddleware implements MiddlewareInterface
+{
+    const MIDDLEWARE_PRIORITY = 7;
+
+    /**
+     * @var TranslatorInterface
+     */
+    private $translator;
+
+    /**
+     * LocaleMiddleware constructor.
+     * @param TranslatorInterface $translator
+     */
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function execute(
+        ClientInterface $client,
+        ServerRequestInterface $request,
+        ManagerInterface $manager
+    ): MiddlewareInterface {
+        $locale = $request->getAttribute('locale', null);
+
+        if (!empty($locale)) {
+            $this->translator->setLocale($locale);
+        }
+
+        return $this;
+    }
+}

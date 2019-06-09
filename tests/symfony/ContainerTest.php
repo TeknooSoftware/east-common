fdfe0fd93051b946d8e9e9209048e06ab4e92af7
Richard Deloge <richarddeloge@gmail.com>
2019-06-09 18:10:15 +0200

    update copyright

--- tests/symfony/ContainerTest.php
+++ tests/symfony/ContainerTest.php
@@ -12,7 +12,7 @@
  * to richarddeloge@gmail.com so we can send you a copy immediately.
  *
  *
- * @copyright   Copyright (c) 2009-2017 Richard Déloge (richarddeloge@gmail.com)
+ * @copyright   Copyright (c) 2009-2019 Richard Déloge (richarddeloge@gmail.com)
  *
  * @link        http://teknoo.software/east/website Project website
  *
@@ -34,7 +34,7 @@ use Teknoo\East\WebsiteBundle\Middleware\LocaleMiddleware;
 /**
  * Class DefinitionProviderTest.
  *
- * @copyright   Copyright (c) 2009-2017 Richard Déloge (richarddeloge@gmail.com)
+ * @copyright   Copyright (c) 2009-2019 Richard Déloge (richarddeloge@gmail.com)
  *
  * @link        http://teknoo.software/east Project website
  *

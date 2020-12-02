5b55bbc51b17fc6ef2ee80bdd552d366b5bab1cf
Richard Déloge <richarddeloge@gmail.com>
2020-12-02 23:08:01 +0100

    WIP

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -858,25 +858,31 @@ class FeatureContext implements Context
 
     private function runSymfony(SFRequest $serverRequest)
     {
-        $this->symfonyKernel->boot();
+        try {
+            $this->symfonyKernel->boot();
 
-        $container = $this->symfonyKernel->getContainer();
+            $container = $this->symfonyKernel->getContainer();
 
-        $container->set(ObjectManager::class, $this->buildObjectManager());
-        $container->set('twig', $this->twig);
+            $container->set(ObjectManager::class, $this->buildObjectManager());
+            $container->set('twig', $this->twig);
 
-        $response = $this->symfonyKernel->handle($serverRequest);
+            $response = $this->symfonyKernel->handle($serverRequest);
 
-        $psrFactory = new \Symfony\Bridge\PsrHttpMessage\Factory\PsrHttpFactory(
-            new \Laminas\Diactoros\ServerRequestFactory(),
-            new \Laminas\Diactoros\StreamFactory(),
-            new \Laminas\Diactoros\UploadedFileFactory(),
-            new \Laminas\Diactoros\ResponseFactory()
-        );
+            $psrFactory = new \Symfony\Bridge\PsrHttpMessage\Factory\PsrHttpFactory(
+                new \Laminas\Diactoros\ServerRequestFactory(),
+                new \Laminas\Diactoros\StreamFactory(),
+                new \Laminas\Diactoros\UploadedFileFactory(),
+                new \Laminas\Diactoros\ResponseFactory()
+            );
 
-        $this->response = $psrFactory->createResponse($response);
+            $this->response = $psrFactory->createResponse($response);
 
-        $this->symfonyKernel->terminate($serverRequest, $response);
+            $this->symfonyKernel->terminate($serverRequest, $response);
+        } catch (\Throwable $error) {
+            \var_dump($error);
+
+            throw $error;
+        }
     }
 
     /**

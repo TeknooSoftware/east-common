48c1f4656e57d7b41c0fb30e84a5ca97cde44e1b
Richard Deloge <richarddeloge@gmail.com>
2023-03-07 09:21:06 +0100

    fix

--- infrastructures/symfony/Recipe/Step/UserTrait.php
+++ infrastructures/symfony/Recipe/Step/UserTrait.php
@@ -1,5 +1,26 @@
 <?php
 
+/*
+ * East Common.
+ *
+ * LICENSE
+ *
+ * This source file is subject to the MIT license
+ * license that are bundled with this package in the folder licences
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to richarddeloge@gmail.com so we can send you a copy immediately.
+ *
+ *
+ * @copyright   Copyright (c) EIRL Richard Déloge (richarddeloge@gmail.com)
+ * @copyright   Copyright (c) SASU Teknoo Software (https://teknoo.software)
+ *
+ * @link        http://teknoo.software/east/common Project website
+ *
+ * @license     http://teknoo.software/license/mit         MIT License
+ * @author      Richard Déloge <richarddeloge@gmail.com>
+ */
+
 declare(strict_types=1);
 
 namespace Teknoo\East\CommonBundle\Recipe\Step;
@@ -12,6 +33,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
  * @license     http://teknoo.software/license/mit         MIT License
  * @author      Richard Déloge <richarddeloge@gmail.com>
  */
+
 trait UserTrait
 {
     private function getUser(): ?UserInterface
